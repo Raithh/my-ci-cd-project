eece257618275630906b1098d5fa56741d39459f
Laith <raith@Laiths-MacBook-Pro.local>
2024-10-23 23:54:05 +0100

    Add HTML and CSS validation setup

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    rules: {
+      "color-no-invalid-hex": true,
+      "font-family-no-duplicate-names": true,
+      "no-empty-source": true,
+      "declaration-block-no-duplicate-properties": true
+    }
+  };
+  
