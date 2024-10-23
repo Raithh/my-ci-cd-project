be1b2490cbfcbf93a2821a829c94c43fb70b887d
Laith <raith@Laiths-MacBook-Pro.local>
2024-10-24 00:25:29 +0100

    Fix ESLint errors by changing string quotes to double quotes

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll("a[href^=\"#\"]").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
         });
     });
 });
