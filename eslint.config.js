a247cb3c23d9115bc21423e589afda18a749039b
Laith <raith@Laiths-MacBook-Pro.local>
2024-10-24 00:22:35 +0100

    Update ESLint configuration to Flat Config format

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,16 @@ module.exports = [
   },
   {
     files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
     rules: {
       "no-console": "warn",       // Warn if there are console statements
       "no-unused-vars": "warn",   // Warn for any unused variables
       "quotes": ["error", "double"], // Enforce double quotes
       "semi": ["error", "always"],   // Enforce semicolons at the end of lines
       "eqeqeq": "error"            // Enforce strict equality
-    },
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
     },
     env: {
       browser: true,
